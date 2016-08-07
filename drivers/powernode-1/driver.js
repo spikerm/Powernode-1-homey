6cb425b815799a86e9eed590d765f37984601ca6
spikerm <rmarkesteijn79@gmail.com>
2016-08-07 11:15:58 +0200

    first upload
    Powernode 1

---
+++ drivers/powernode-1/driver.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const path			= require('path');
+const ZwaveDriver	= require('homey-zwavedriver');
+
+// http://www.pepper1.net/zwavedb/device/280
+
+module.exports = new ZwaveDriver( path.basename(__dirname), {
+	//debug: true,
+	capabilities: {
+		'onoff': {
+			'command_class'				: 'COMMAND_CLASS_SWITCH_BINARY',
+			'command_get'				: 'SWITCH_BINARY_GET',
+			'command_get_cb'			: false,
+			'command_set'				: 'SWITCH_BINARY_SET',
+			'command_set_parser'		: function( value ){
+				return {
+					'Switch Value': value
+				}
+			},
+			'command_report'			: 'SWITCH_BINARY_REPORT',
+			'command_report_parser'		: function( report ){
+				return report['Value'] === 'on/enable';
+			},
+			'pollInterval': "poll_interval"
+		},
+		'meter_power': {
+			'command_class'				: 'COMMAND_CLASS_METER',
+			'command_get'				: 'METER_GET',
+			'command_get_cb'			: false,
+			'command_get_parser'		: function(power){
+				return {
+					'Properties1': {
+						'Scale': 0
+					}
+				}
+			},
+			'command_report'			: 'METER_REPORT',
+			'command_report_parser'		: function( report ) {
+				return report['Meter Value (Parsed)'];
+				
+			},
+			'pollInterval': "poll_interval"
+		}
+	},
+	settings: {}
+})
